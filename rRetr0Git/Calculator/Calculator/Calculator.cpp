d371ee2e6dcbbb5f8f75d5f51a9ac8cbfe04238a
Retr0 <923898595@qq.com>
2019-02-27 20:31:29 +0800

    fin

---
+++ rRetr0Git/Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,139 @@
+﻿
+
+#include "pch.h"
+#include <stack>
+#include <vector>
+#include <iostream>
+#include "stdlib.h"
+#include <ctime>
+#include <string>
+#include "Calculator.h"
+#include "time.h"
+#include<fstream>
+#include<iomanip>
+#define random(a,b) (rand()%(b-a+1)+a)
+
+using namespace std;
+
+Calculator::Calculator() {}
+
+string Calculator::MakeFormula() {
+	string formula = "";
+	int count = random(1, 2);
+	int start = 1;
+	int number1 = random(0, 100);
+	formula += to_string(number1);
+	int pre = number1;
+	while (start <= count) {
+		int operation = random(0, 3);
+		int number2 = random(0, 100);
+		if (operation == 3) {
+			while (number2==0||pre%number2 != 0) {
+				number2 = random(0, 100);
+			}
+			pre = pre / number2;
+		}
+		else if (operation == 2) {
+			pre = number1 * number2;
+		}
+		else {
+			pre = number2;
+		}
+		formula += op[operation] + to_string(number2);
+		start++;
+	}
+	return formula;
+};
+
+string Calculator::Solve(string formula) {
+	vector<string>* tempStack = new vector<string>();
+	stack<char>* operatorStack = new stack<char>();
+	int len = formula.length();
+	int k = 0;
+	for (int j = -1; j < len - 1; j++) {
+		char formulaChar = formula[j + 1];
+		if (j == len - 2 || formulaChar == '+' || formulaChar == '-' ||
+			formulaChar == '*' || formulaChar == '/') {
+			if (j == len - 2) {
+				tempStack->push_back(formula.substr(k));
+			}
+			else {
+				if (k <=j) {
+					tempStack->push_back(formula.substr(k, j - k+ 1));
+				}
+				if (operatorStack->empty()) {
+					operatorStack->push(formulaChar);
+				}
+				else {
+					char stackChar = operatorStack->top();
+					if ((stackChar == '+' || stackChar == '-')
+						&& (formulaChar == '*' || formulaChar == '/')) {
+						operatorStack->push(formulaChar);
+					}
+					else {
+						tempStack->push_back(string(1,operatorStack->top()));
+						operatorStack->pop();
+						operatorStack->push(formulaChar);
+					}
+				}
+			}
+			k = j + 2;
+		}
+	}
+	while (!operatorStack->empty()) {
+		tempStack->push_back(string(1, operatorStack->top()));
+		operatorStack->pop();
+	}
+	stack<string>* calcStack = new stack<string>();
+	for (int i = 0; i < tempStack->size(); i++) {
+		string peekChar = tempStack->at(i);
+		if (peekChar != "+" && peekChar != "-"
+			&& peekChar != "/" && peekChar != "*") {
+			calcStack->push(peekChar);
+		}
+		else {
+			int a1 = 0;
+			int b1 = 0;
+			if (!calcStack->empty()) {
+				b1 = stoi(calcStack->top());
+				calcStack->pop();
+			}
+			if (!calcStack->empty()) {
+				a1 = stoi(calcStack->top());
+				calcStack->pop();
+			}
+			if (peekChar == "+") {
+				calcStack->push(to_string(a1 + b1));
+			}
+			else if (peekChar == "-") {
+				calcStack->push(to_string(a1 - b1));
+			}
+			else if (peekChar == "*") {
+				calcStack->push(to_string(a1 * b1));
+			}
+			else if (peekChar == "/") {
+				calcStack->push(to_string(a1 / b1));
+			}
+		}
+	}
+	return formula + "=" + calcStack->top();
+}
+
+int main()
+{
+	ofstream ofile; 
+	ofile.open("subject.txt");
+	Calculator* calc = new Calculator();
+	int n;
+	cin >> n;
+	string question;
+	srand((unsigned int)time(NULL) );
+	while (n--) {
+		question = calc->MakeFormula();
+		string ret = calc->Solve(question);
+		ofile << ret << endl;
+	}
+	ofile.close();
+}
+
+
