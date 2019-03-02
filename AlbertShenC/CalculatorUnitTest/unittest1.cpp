aec93de681f4a38f31fe088f6edfcc95e6858c4c
AlbertShenC <30514009+AlbertShenC@users.noreply.github.com>
2019-03-02 11:19:20 +0800

    Local version

--- AlbertShenC/CalculatorUnitTest/unittest1.cpp
+++ AlbertShenC/CalculatorUnitTest/unittest1.cpp
@@ -4,6 +4,8 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+// This is local version
+
 namespace CalculatorUnitTest
 {		
 	TEST_CLASS(UnitTest1)
@@ -13,8 +15,8 @@ namespace CalculatorUnitTest
 		TEST_METHOD(TestMethod1)
 		{
 			Calculator* calc = new Calculator();
-			string ret = calc->Solve("11+22");
-			Assert::AreEqual(ret, (string)"11+22=33");
+			string ret = calc->Solve("11+32");
+			Assert::AreEqual(ret, (string)"11+32=33");
 		}
 
 		TEST_METHOD(TestMethod2)
