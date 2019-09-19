6c9d3cab8ebe7f2b4a325be8feaedd42b015b008
ChengXiang <2363453982@qq.com>
2019-09-19 22:52:01 +0800

    chengxiang

---
+++ chengxiang/ConsoleApplication1/CalculatorUnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            calculator cc = new calculator();
+           
+        }
+    }
+}
