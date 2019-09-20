42147d4b0f9619e6eec318a1d9e5cbc5f1aff318
gy722 <1925392268@qq.com>
2019-09-20 14:33:17 +0800

    First Commit

---
+++ gy722/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Data;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int expect = 3;
+            int[] numbers = new int[3] { 1, 2, 3 };
+            Random num = new Random();
+            int res = Random.Largest(numbers);
+            Assert.AreEqual(res, expect);
+        }
+    }
+}
