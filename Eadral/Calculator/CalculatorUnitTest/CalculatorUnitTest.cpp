7983ea650cd0baf75b34fba6af7be1229d755b95
Eadral <a250014238@me.com>
2020-02-28 18:29:57 +0800

    fixed a bug.

--- Eadral/Calculator/CalculatorUnitTest/CalculatorUnitTest.cpp
+++ Eadral/Calculator/CalculatorUnitTest/CalculatorUnitTest.cpp
@@ -10,11 +10,17 @@ namespace CalculatorUnitTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Test1)
 		{
 			Calculator* calc = new Calculator();
 			string ret = calc->Solve("11+22");
 			Assert::AreEqual(ret, (string)"11+22=33");
+		}
+		TEST_METHOD(Test2)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("87/1*2/1");
+			Assert::AreEqual(ret, (string)"87/1*2/1=174");
 		}
 	};
 }
