2880aaa4437a8b9385a970e5c46e44a1c71d45dc
苏海翔 <17373072@buaa.edu.cn>
2020-03-01 19:17:34 +0800

    初次提交，实现基本功能，未进行性能检测

---
+++ LastWhisper1/UnitTest1/CalculatorUnitTest.cpp
@@ -0,0 +1,56 @@
+#include "pch.h"
+#include "../Calculator/calculator.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11+22");
+			Assert::AreEqual(ret, (string)"11+22=33");
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11*6");
+			Assert::AreEqual(ret, (string)"11*6=66");
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11+22+1");
+			Assert::AreEqual(ret, (string)"11+22+1=34");
+		}
+	};
+
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11-22+6/3");
+			Assert::AreEqual(ret, (string)"11-22+6/3=-9");
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11*2/2*9");
+			Assert::AreEqual(ret, (string)"11*2/2*9=99");
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("32*3-12*8");
+			Assert::AreEqual(ret, (string)"32*3-12*8=0");
+		}
+	};
+}
