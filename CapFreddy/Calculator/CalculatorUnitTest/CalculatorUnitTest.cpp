264ec12ff50c67c4c6ba84c0ce30b5ffb5b2ac70
CapFreddy <fred9918@hotmail.com>
2020-03-02 01:23:17 +0800

    just a test

---
+++ CapFreddy/Calculator/CalculatorUnitTest/CalculatorUnitTest.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Calculator/Calculator.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorUnitTest
+{
+	TEST_CLASS(CalculatorUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Calculator* calc = new Calculator();
+			Assert::AreEqual(calc->Solve("13+17-1"), (string)"13+17-1=29");
+			Assert::AreEqual(calc->Solve("11*15-5"), (string)"11*15-5=160");
+			Assert::AreEqual(calc->Solve("3+10+4-16"), (string)"3+10+4-16=1");
+			Assert::AreEqual(calc->Solve("15/5+3-2"), (string)"15/5+3-2=4");
+		}
+	};
+}
