a87640aadb4c45e68d79aada62515f2a7b8024cd
李沛熙 <17231162@buaa.edu.cn>
2020-03-01 23:21:49 +0800

    fix bugs

--- lpxofbuaa/Calculator/Calculator/Calculator.cpp
+++ lpxofbuaa/Calculator/Calculator/Calculator.cpp
@@ -1,6 +1,4 @@
-﻿
-
-#include "stdafx.h"
+﻿//#include "stdafx.h"
 #include <stack>
 #include <vector>
 #include <iostream>
@@ -15,16 +13,88 @@ using namespace std;
 
 Calculator::Calculator() {}
 
+bool Calculator::isPrime(int num) {
+	
+	if (num < 3)
+		return num >= 1;
+		
+	if (num % 6 != 1 && num % 6 != 5)
+		return false;
+	int sq = (int)sqrt(num);
+	for (int i = 5; i <= sq; i += 6)
+	{
+		if (num % i == 0 || num % (i + 2) == 0)
+			return false;
+	}
+	return true;
+	
+}
+
 string Calculator::MakeFormula() {
 	string formula = "";
-	srand((unsigned int)time(NULL));
+	
 	int count = random(1, 3);
 	int start = 0;
-	int number1 = random(1, 100);
+	int number1 = random(0, 100);
+	int tmp = number1;
 	formula += to_string(number1);
 	while (start <= count) {
 		int operation = random(0, 3);
-		int number2 = random(1, 100);
+		int number2 = random(0, 100);
+		if (op[operation] == "+" || op[operation] == "-") {
+			tmp = number2;
+		}
+		else if (op[operation] == "*") {
+			tmp *= number2;
+		}
+		if (op[operation] == "/") {
+			if (tmp != 0 && isPrime(tmp)) {
+				if (number2 % 2 == 0 || tmp > 100) {
+					number2 = 1;
+				}
+				else {
+					number2 = tmp;
+				}
+			}
+			else {
+				int max = 100;
+				int min = 1;
+				do {
+					number2 = random(min, max);
+					if (tmp % number2 == 0) {
+						break;
+					}
+					else {
+						if (100 < sqrt(tmp)) {
+							if (max - number2 > number2 - min) {
+								min = number2 + 1;
+							}
+							else {
+								max = number2 - 1;
+							}
+						}
+						else {
+							if (number2 > sqrt(tmp)) {
+								max = number2 - 1;
+							}
+							else {
+								min = number2+ 1;
+							}
+						}
+					}
+				} while (min < max);
+				if (min >= max) {
+					if (number2 % 2 == 0 || tmp > 100) {
+						number2 = 1;
+					}
+					else {
+						number2 = tmp;
+					}
+				}
+			}
+			
+		}
+		//cout << number2 << endl;
 		formula += op[operation] + to_string(number2);
 		start++;
 	}
@@ -44,23 +114,26 @@ string Calculator::Solve(string formula) {
 				tempStack->push_back(formula.substr(k));
 			}
 			else {
-				if (k < j) {
-					tempStack->push_back(formula.substr(k, j + 1));
+				if (k <= j) {
+					tempStack->push_back(formula.substr(k, j + 1 - k));
 				}
 				if (operatorStack->empty()) {
 					operatorStack->push(formulaChar);
 				}
 				else {
-					char stackChar = operatorStack->top();
-					if ((stackChar == '+' || stackChar == '-')
-						&& (formulaChar == '*' || formulaChar == '/')) {
-						operatorStack->push(formulaChar);
+					while (not operatorStack->empty()) {
+						char stackChar = operatorStack->top();
+						if ((stackChar == '+' || stackChar == '-')
+							&& (formulaChar == '*' || formulaChar == '/')) {
+							break;
+						}
+						else {
+							tempStack->push_back(string(1, operatorStack->top()));
+							operatorStack->pop();
+							
+						}
 					}
-					else {
-						tempStack->push_back(to_string(operatorStack->top()));
-						operatorStack->pop();
-						operatorStack->push(formulaChar);
-					}
+					operatorStack->push(formulaChar);
 				}
 			}
 			k = j + 2;
@@ -105,14 +178,41 @@ string Calculator::Solve(string formula) {
 	return formula + "=" + calcStack->top();
 }
 
+QuestionBuilder::QuestionBuilder(Calculator *calc) {
+	this->calc = calc;
+	writer = ofstream(FILENAME);
+	if (writer.fail()) {
+		cerr << "Open file fail!" << endl;
+		exit(-1);
+	}
+}
+
+void QuestionBuilder::Make(int n) {
+	srand((unsigned int)time(NULL));
+	this->total = n;
+	string question;
+	string ret;
+	for (int i = 0; i < this->total; ++i) {
+		question = calc->MakeFormula();
+		ret = calc->Solve(question);
+		writer << ret << endl;
+		//cout << ret << endl;
+	}
+	writer.close();
+}
+
 int main()
 {
 	Calculator* calc = new Calculator();
-	string question = calc->MakeFormula();
+	QuestionBuilder* builder = new QuestionBuilder(calc);
+	int n;
+	cin >> n;
+	builder->Make(n);
+	/*string question = calc->MakeFormula();
 	cout << question << endl;
-	string ret = calc->Solve("11+22");
-	cout << ret << endl;
-	getchar();
+	string ret = calc->Solve("3*60-2");
+	cout << ret << endl;*/
+	//getchar();
 }
 
 
