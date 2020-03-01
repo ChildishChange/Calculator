d864d3d1e01470ed42d12f01d690d3380c15ac44
刘阳 <17373118@buaa.edu.cn>
2020-03-01 16:07:52 +0800

    test

---
+++ Pupil59/Calculator/CalculatorUnitTest/CalculatorUnitTest.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Calculator/calculator.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorUnitTest
+{
+	TEST_CLASS(CalculatorUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11+22");
+			Assert::AreEqual(ret, (string)"11+22=33");
+			ret = calc->Solve("11+22");
+			Assert::AreEqual(ret, (string)"11+22=33");
+			ret = calc->Solve("11+22/11");
+			Assert::AreEqual(ret, (string)"11+22/11=13");
+			ret = calc->Solve("11*22");
+			Assert::AreEqual(ret, (string)"11*22=242");
+			ret = calc->Solve("22/11*2");
+			Assert::AreEqual(ret, (string)"22/11*2=4");
+			ret = calc->Solve("11*22-200+3");
+			Assert::AreEqual(ret, (string)"11*22-200+3=45");
+			ret = calc->Solve("0/22");
+			Assert::AreEqual(ret, (string)"0/22=0");
+			ret = calc->Solve("100*100");
+			Assert::AreEqual(ret, (string)"100*100=10000");
+		}
+	};
+}
