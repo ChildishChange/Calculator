c95411a70a8335a50963e494d08f39801ea95cce
苏海翔 <17373072@buaa.edu.cn>
2020-03-02 20:30:04 +0800

    将tab缩进改成四个空格

--- LastWhisper1/Calculator/Calculator.h
+++ LastWhisper1/Calculator/Calculator.h
@@ -12,27 +12,27 @@ using namespace std;
 
 class Calculator {
 private:
-	string op[4] = { "+", "-", "*", "/" };
+    string op[4] = { "+", "-", "*", "/" };
 public:
-	Calculator();
-	string MakeFormula();
-	string Solve(string formula);
+    Calculator();
+    string MakeFormula();
+    string Solve(string formula);
 };
 
 int findFactor(int num) {
-	//对一个100以内的正整数尝试寻找因子
-	//如果是合数，则在2~10以内随机返回一个因子
-	//如果是质数，返回1
-	int farr[9] = {0};
-	int fcnt = 0;
-	for (int i = 2; i <= 10; i++) {
-		if (num % i == 0 && num != i) {
-			farr[fcnt++] = i;
-		}
-	}
-	if (fcnt == 0) return 1;
-	else {
-		//srand((unsigned int)time(NULL));
-		return farr[random(0, fcnt-1)];
-	}
+    //对一个100以内的正整数尝试寻找因子
+    //如果是合数，则在2~10以内随机返回一个因子
+    //如果是质数，返回1
+    int farr[9] = {0};
+    int fcnt = 0;
+    for (int i = 2; i <= 10; i++) {
+        if (num % i == 0 && num != i) {
+            farr[fcnt++] = i;
+        }
+    }
+    if (fcnt == 0) return 1;
+    else {
+        //srand((unsigned int)time(NULL));
+        return farr[random(0, fcnt-1)];
+    }
 }
--- LastWhisper1/UnitTest1/CalculatorUnitTest.cpp
+++ LastWhisper1/UnitTest1/CalculatorUnitTest.cpp
@@ -6,51 +6,51 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
-	TEST_CLASS(UnitTest1)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			Calculator* calc = new Calculator();
-			string ret = calc->Solve("11+22");
-			Assert::AreEqual(ret, (string)"11+22=33");
-		}
-		TEST_METHOD(TestMethod2)
-		{
-			Calculator* calc = new Calculator();
-			string ret = calc->Solve("11*6");
-			Assert::AreEqual(ret, (string)"11*6=66");
-		}
-		TEST_METHOD(TestMethod3)
-		{
-			Calculator* calc = new Calculator();
-			string ret = calc->Solve("11+22+1");
-			Assert::AreEqual(ret, (string)"11+22+1=34");
-		}
-	};
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        
+        TEST_METHOD(TestMethod1)
+        {
+            Calculator* calc = new Calculator();
+            string ret = calc->Solve("11+22");
+            Assert::AreEqual(ret, (string)"11+22=33");
+        }
+        TEST_METHOD(TestMethod2)
+        {
+            Calculator* calc = new Calculator();
+            string ret = calc->Solve("11*6");
+            Assert::AreEqual(ret, (string)"11*6=66");
+        }
+        TEST_METHOD(TestMethod3)
+        {
+            Calculator* calc = new Calculator();
+            string ret = calc->Solve("11+22+1");
+            Assert::AreEqual(ret, (string)"11+22+1=34");
+        }
+    };
 
-	TEST_CLASS(UnitTest2)
-	{
-	public:
+    TEST_CLASS(UnitTest2)
+    {
+    public:
 
-		TEST_METHOD(TestMethod1)
-		{
-			Calculator* calc = new Calculator();
-			string ret = calc->Solve("11-22+6/3");
-			Assert::AreEqual(ret, (string)"11-22+6/3=-9");
-		}
-		TEST_METHOD(TestMethod2)
-		{
-			Calculator* calc = new Calculator();
-			string ret = calc->Solve("11*2/2*9");
-			Assert::AreEqual(ret, (string)"11*2/2*9=99");
-		}
-		TEST_METHOD(TestMethod3)
-		{
-			Calculator* calc = new Calculator();
-			string ret = calc->Solve("32*3-12*8");
-			Assert::AreEqual(ret, (string)"32*3-12*8=0");
-		}
-	};
+        TEST_METHOD(TestMethod1)
+        {
+            Calculator* calc = new Calculator();
+            string ret = calc->Solve("11-22+6/3");
+            Assert::AreEqual(ret, (string)"11-22+6/3=-9");
+        }
+        TEST_METHOD(TestMethod2)
+        {
+            Calculator* calc = new Calculator();
+            string ret = calc->Solve("11*2/2*9");
+            Assert::AreEqual(ret, (string)"11*2/2*9=99");
+        }
+        TEST_METHOD(TestMethod3)
+        {
+            Calculator* calc = new Calculator();
+            string ret = calc->Solve("32*3-12*8");
+            Assert::AreEqual(ret, (string)"32*3-12*8=0");
+        }
+    };
 }
