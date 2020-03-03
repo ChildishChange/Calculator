2d20cd7bed303bdcb141dae3e922c31d6474672e
CSDarkCounter <406155679@qq.com>
2020-03-03 18:06:07 +0800

    last

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -15,50 +15,55 @@ using namespace std;
 
 Calculator::Calculator() {}
 
-string Calculator::MakeFormula() {
+string Calculator::MakeFormula(){
+
 	string formula = "";
 	srand((unsigned int)time(NULL));
 	int count = random(1, 3);
 	int start = 0;
 	int number1 = random(1, 100);
 	formula += to_string(number1);
-	while (start <= count) {
+	while (start <= count){
 		int operation = random(0, 3);
 		int number2 = random(1, 100);
 		formula += op[operation] + to_string(number2);
 		start++;
 	}
 	return formula;
+
 };
 
-string Calculator::Solve(string formula) {
+string Calculator::Solve(string formula){
+
 	vector<string>* tempStack = new vector<string>();
 	stack<char>* operatorStack = new stack<char>();
 	int len = formula.length();
 	int k = 0;
-	for (int j = -1; j < len - 1; j++) {
+	for (int j = -1; j < len - 1; j++){
 		char formulaChar = formula[j + 1];
 		if (j == len - 2 || formulaChar == '+' || formulaChar == '-' ||
-			formulaChar == '*' || formulaChar == '/') {
-			if (j == len - 2) {
+			formulaChar == '*' || formulaChar == '/'){
+			if (j == len - 2){
 				tempStack->push_back(formula.substr(k));
 			}
-			else {
-				if (k < j) {
-					tempStack->push_back(formula.substr(k, j + 1));
+			else{
+				if (k < j + 1){
+					tempStack->push_back(formula.substr(k, j + 1 - k));
 				}
-				if (operatorStack->empty()) {
+				if (operatorStack->empty()){
 					operatorStack->push(formulaChar);
 				}
-				else {
+				else{
 					char stackChar = operatorStack->top();
 					if ((stackChar == '+' || stackChar == '-')
-						&& (formulaChar == '*' || formulaChar == '/')) {
+						&& (formulaChar == '*' || formulaChar == '/')){
 						operatorStack->push(formulaChar);
 					}
-					else {
-						tempStack->push_back(to_string(operatorStack->top()));
-						operatorStack->pop();
+					else{
+						while (!operatorStack->empty()){
+							tempStack->push_back(string(1, operatorStack->top()));
+							operatorStack->pop();
+						}
 						operatorStack->push(formulaChar);
 					}
 				}
@@ -66,38 +71,39 @@ string Calculator::Solve(string formula) {
 			k = j + 2;
 		}
 	}
-	while (!operatorStack->empty()) {
+	while (!operatorStack->empty()){
 		tempStack->push_back(string(1, operatorStack->top()));
 		operatorStack->pop();
 	}
+
 	stack<string>* calcStack = new stack<string>();
-	for (int i = 0; i < tempStack->size(); i++) {
+	for (int i = 0; i < tempStack->size(); i++){
 		string peekChar = tempStack->at(i);
 		if (peekChar != "+" && peekChar != "-"
-			&& peekChar != "/" && peekChar != "*") {
+			&& peekChar != "/" && peekChar != "*"){
 			calcStack->push(peekChar);
 		}
-		else {
+		else{
 			int a1 = 0;
 			int b1 = 0;
-			if (!calcStack->empty()) {
+			if (!calcStack->empty()){
 				b1 = stoi(calcStack->top());
 				calcStack->pop();
 			}
-			if (!calcStack->empty()) {
+			if (!calcStack->empty()){
 				a1 = stoi(calcStack->top());
 				calcStack->pop();
 			}
-			if (peekChar == "+") {
+			if (peekChar == "+"){
 				calcStack->push(to_string(a1 + b1));
 			}
-			else if (peekChar == "-") {
+			else if (peekChar == "-"){
 				calcStack->push(to_string(a1 - b1));
 			}
-			else if (peekChar == "*") {
+			else if (peekChar == "*"){
 				calcStack->push(to_string(a1 * b1));
 			}
-			else if (peekChar == "/") {
+			else if (peekChar == "/"){
 				calcStack->push(to_string(a1 / b1));
 			}
 		}
@@ -110,9 +116,10 @@ int main()
 	Calculator* calc = new Calculator();
 	string question = calc->MakeFormula();
 	cout << question << endl;
-	string ret = calc->Solve("11+22");
+	string ret = calc->Solve("12+22-1");
 	cout << ret << endl;
-	getchar();
+	
 }
+
 
 
