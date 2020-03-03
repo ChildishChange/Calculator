4da7db2a8bee84dc012c3a95a57434c7cd926838
mjjm3 <1508929687@qq.com>
2020-03-03 21:58:53 +0800

    test

---
+++ mjjm3/Cal/CalUnitTest/CalUnitTest.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Cal/Calculator.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalUnitTest
+{
+	TEST_CLASS(CalUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Calculator* calc = new Calculator();
+			string ret = calc->Solve("11+22");
+			Assert::AreEqual(ret, (string)"11+22=33");
+		}
+	};
+}
